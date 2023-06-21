aa2150f437443d9db59cf2f296ac9fd9488d2a52
Viktor <vitya.getman.79@gmail.com>
2023-06-21 15:00:34 +0200

    reusable button component + types

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,35 @@
+"use client";
+
 import React from "react";
+import Image from "next/image";
+
+import Button from "./Button";
 
 const Hero: React.FC = () => {
+  const handleScroll = () => {};
   return (
     <div className="hero">
-      <div className="flex-1 pt-36 padding-x"></div>
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, book, or rent a car — quickly and easily!
+        </h1>
+
+        <p className="hero_subtitle">
+          Streamline your car rental experience with our effortless booking
+          process
+        </p>
+
+        <Button
+          title="Explore cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src={} />
+        </div>
+      </div>
     </div>
   );
 };
