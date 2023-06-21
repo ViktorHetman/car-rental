c97332f536bcc20cb0a21385d3910658dc0eb8f8
Viktor <vitya.getman.79@gmail.com>
2023-06-21 18:42:39 +0200

    custom components + layout editing

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CustomFilter: React.FC<{ title: string }> = ({ title }) => {
+  return <div>CustomFilter</div>;
+};
+
+export default CustomFilter;
--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,7 @@ import Hero from "./Hero";
 import Button from "./Button";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
 
-export { Hero, Button, Navbar, Footer };
+export { Hero, Button, Navbar, Footer, SearchBar, CustomFilter };
