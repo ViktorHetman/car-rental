6e8e7d01d83b3733cf84a49b7449688271c44b2e
Viktor <vitya.getman.79@gmail.com>
2023-06-21 14:25:17 +0200

    tailwind config + globals + file for components exports

---
+++ components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero: React.FC = () => {
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x"></div>
+    </div>
+  );
+};
+
+export default Hero;
